3f442e208f061ad6ceff1fa0712997b13f3d35ff
yazn1q3 <yaznal773@gmail.com>
2023-12-04 18:27:30 +0300

    zz

--- app/(public)/layout.tsx
+++ app/(public)/layout.tsx
@@ -1,15 +1,10 @@
-import Header from "@/components/header";
-
-
 const PublicLayout = ({
     children
   }: {
     children: React.ReactNode;
   }) => {
-  
     return ( 
       <div className="h-full dark:bg-[#1F1F1F]">
-      <Header />
         {children}
       </div>
      );
