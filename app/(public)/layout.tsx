63b16a0ed2f46481194b74a295d11ba65deec954
yazn1q3 <yaznal773@gmail.com>
2023-12-04 18:22:02 +0300

    Fixed

--- app/(public)/layout.tsx
+++ app/(public)/layout.tsx
@@ -1,61 +1,15 @@
-import { ModeToggle } from "@/components/mode-toggle";
-import { Button } from "@/components/ui/button";
-import { useScrollTop } from "@/hooks/use-scroll-top";
-import { SignInButton, UserButton } from "@clerk/clerk-react";
-import { useConvexAuth } from "convex/react";
-import Link from "next/link";
-import { Logo } from "../(marketing)/_components/logo";
-import { Spinner } from "@/components/spinner";
-import { cn } from "@/lib/utils";
+import Header from "@/components/header";
+
 
 const PublicLayout = ({
     children
   }: {
     children: React.ReactNode;
   }) => {
-    const { isAuthenticated, isLoading } = useConvexAuth();
-    const scrolled = useScrollTop();
   
     return ( 
       <div className="h-full dark:bg-[#1F1F1F]">
-        <div className={cn(
-      "z-50 bg-background dark:bg-[#1F1F1F] fixed top-0 flex items-center w-full p-6",
-      scrolled && "border-b shadow-sm"
-    )}>
-      <Logo />
-      <div className="md:ml-auto md:justify-end justify-between w-full flex items-center gap-x-2">
-        {isLoading && (
-          <Spinner />
-        )}
-        {!isAuthenticated && !isLoading && (
-          <>
-            <SignInButton mode="modal">
-              <Button variant="ghost" size="sm">
-                Log in
-              </Button>
-            </SignInButton>
-            <SignInButton mode="modal">
-              <Button size="sm">
-                Get Yaznbook free
-              </Button>
-            </SignInButton>
-          </>
-        )}
-        {isAuthenticated && !isLoading && (
-          <>
-            <Button variant="ghost" size="sm" asChild>
-              <Link href="/documents">
-                Enter Yaznbook
-              </Link>
-            </Button>
-            <UserButton
-              afterSignOutUrl="/"
-            />
-          </>
-        )}
-        <ModeToggle />
-      </div>
-    </div>
+      <Header />
         {children}
       </div>
      );
---
+++ components/header.tsx
@@ -0,0 +1,57 @@
+import { ModeToggle } from "@/components/mode-toggle";
+import { Button } from "@/components/ui/button";
+import { useScrollTop } from "@/hooks/use-scroll-top";
+import { SignInButton, UserButton } from "@clerk/clerk-react";
+import { useConvexAuth } from "convex/react";
+import Link from "next/link";
+
+import { Spinner } from "@/components/spinner";
+import { cn } from "@/lib/utils";
+import { Logo } from "@/app/(marketing)/_components/logo";
+
+const Header = () => {
+    const { isAuthenticated, isLoading } = useConvexAuth();
+    const scrolled = useScrollTop();
+  return (
+    <div className={cn(
+        "z-50 bg-background dark:bg-[#1F1F1F] fixed top-0 flex items-center w-full p-6",
+        scrolled && "border-b shadow-sm"
+      )}>
+        <Logo />
+        <div className="md:ml-auto md:justify-end justify-between w-full flex items-center gap-x-2">
+          {isLoading && (
+            <Spinner />
+          )}
+          {!isAuthenticated && !isLoading && (
+            <>
+              <SignInButton mode="modal">
+                <Button variant="ghost" size="sm">
+                  Log in
+                </Button>
+              </SignInButton>
+              <SignInButton mode="modal">
+                <Button size="sm">
+                  Get Yaznbook free
+                </Button>
+              </SignInButton>
+            </>
+          )}
+          {isAuthenticated && !isLoading && (
+            <>
+              <Button variant="ghost" size="sm" asChild>
+                <Link href="/documents">
+                  Enter Yaznbook
+                </Link>
+              </Button>
+              <UserButton
+                afterSignOutUrl="/"
+              />
+            </>
+          )}
+          <ModeToggle />
+        </div>
+      </div>
+  )
+}
+
+export default Header;
