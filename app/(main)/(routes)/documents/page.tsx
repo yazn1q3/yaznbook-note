5b8bc8e2e39754de607cf11438123d30a533a88c
yazn1q3 <yaznal773@gmail.com>
2024-01-03 20:38:27 +0300

    versionena

--- app/(main)/(routes)/documents/page.tsx
+++ app/(main)/(routes)/documents/page.tsx
@@ -1,3 +1,5 @@
+
+
 "use client";
 
 import Image from "next/image";
@@ -9,11 +11,35 @@ import { useRouter } from "next/navigation";
 
 import { api } from "@/convex/_generated/api";
 import { Button } from "@/components/ui/button";
+import { Avatar, AvatarImage } from "@/components/ui/avatar";
+import { Label } from "@/components/ui/label";
+import { useEffect, useState } from "react";
+import { Separator } from "@/components/ui/separator";
 
 const DocumentsPage = () => {
   const router = useRouter();
   const { user } = useUser();
   const create = useMutation(api.documents.create);
+  const [sss, setSsss] = useState('');
+  const [greeting, setGreeting] = useState('');
+  
+
+
+
+  useEffect(() => {
+    const now = new Date();
+    const hour = now.getHours();
+    let message = '';
+
+    if (hour >= 0 && hour < 12) {
+      message = 'Good Moring!';
+    } else {
+      message = 'Good evening';
+    }
+
+    setGreeting(message);
+  }, []);
+
 
 
   const onCreate = () => {
@@ -30,9 +56,28 @@ const DocumentsPage = () => {
   return ( 
     <>
     
-    <div className="h-full flex flex-col items-center justify-center space-y-4">
- 
-      <Image
+    <div className="w-[100%]">
+ <div className="flex items-center p-2" >
+ <Avatar className="w-[30px] h-[30px] m-2 rounded-[50px]" >
+  <AvatarImage src={user?.imageUrl} />
+ </Avatar>
+ <Label>Hello {user?.fullName}&apos;s {greeting}</Label>
+ </div>
+  
+    </div>
+
+    <div>
+      
+      <Separator />
+    
+
+<div className="m-3" />
+     <br />
+
+     <br />
+     <br />
+  <div className="h-full flex flex-col items-center justify-center space-y-4">
+  <Image
         src="/empty.png"
         height="300"
         width="300"
@@ -46,14 +91,17 @@ const DocumentsPage = () => {
         alt="Empty"
         className="hidden dark:block"
       />
-      <h2 className="text-lg font-medium">
-        Welcome to {user?.firstName}&apos;s Yaznbook
-      </h2>
-      <Button className=" rounded-[10px]"  onClick={onCreate}>
-        <PlusCircle className="h-4 w-4 mr-2" />
-        Create a Note
-      </Button>
-    </div></>
+ <h2 className="text-lg font-medium">
+   Welcome to {user?.firstName}&apos;s Yaznbook
+ </h2>
+ <Button className=" rounded-[10px]"  onClick={onCreate}>
+   <PlusCircle className="h-4 w-4 mr-2" />
+   Create a Note
+ </Button>
+
+      </div>
+      </div>
+    </>
    );
 }
  
